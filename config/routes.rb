323430a6c0b87623be298d7bc8fbda372d3ce9b3
Pedro Cobos <pedro.cobos@live.com>
2020-09-13 18:07:11 -0500

    added search stock route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root to: "welcome#index"
   get 'users/my_portfolio', to: 'users#my_portfolio', as: "my_portfolio"
+  get 'search_stock', to: 'stocks#search'
+
 end
