54cb84e42ed5d788d6eff91cfd0d80038027c34a
Pedro Cobos <pedro.cobos@live.com>
2020-09-15 23:56:08 -0500

    added comments for invalid search exception

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,8 @@ class Stock < ApplicationRecord
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    # client.price(ticker_symbol)
+
+    # The following is an exception to handle invalid ticker searches (other than empty). Basically, if it doesn't find the symbol, it will return nil and then it will render the portfolio form again. It's similar to a conditional in a way.
     begin
       new(name: client.company(ticker_symbol).company_name , ticker: ticker_symbol, last_price: client.price(ticker_symbol))
     rescue => exception
