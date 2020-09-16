31b4a6f5cc7557c6721cab8f8b00beac3fe4a877
Pedro Cobos <pedro.cobos@live.com>
2020-09-15 23:50:13 -0500

    fixed API invalid ticker issue

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,7 @@ class Stock < ApplicationRecord
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    client.price(ticker_symbol)
+    # client.price(ticker_symbol)
     begin
       new(name: client.company(ticker_symbol).company_name , ticker: ticker_symbol, last_price: client.price(ticker_symbol))
     rescue => exception
