ab5a016916c64965f2892deb09e0abd5102603ce
Pedro Cobos <pedro.cobos@live.com>
2020-09-18 18:53:55 -0500

    ran migration to add user_stocks table to schema

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
-  has_many :users through :user_stocks
+  has_many :users, through: :user_stocks
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
