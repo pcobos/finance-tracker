f8e12ad692959be3c394b908ad707576cdba3a21
Pedro Cobos <pedro.cobos@live.com>
2020-09-26 18:37:29 -0500

    added a notice for successful stock tracking and redirect to portfolio path

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -4,7 +4,10 @@ class UserStocksController < ApplicationController
     stock = Stock.check_db(params[:ticker])
     if stock.blank?
       stock = Stock.new_lookup(params[:ticker])
+      stock.save
       @user_stock = UserStock.create(user: current_user,  stock: stock)
+      flash[:notice] = "Stock #{stock.name} was succesfully added to your portfolio"
+      redirect_to my_portfolio_path
     end
   end
 end
