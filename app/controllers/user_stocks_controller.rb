edabcb78a369e6ce7bb2508dafa6f92d57f0a9f1
Pedro Cobos <pedro.cobos@live.com>
2020-09-25 22:43:55 -0500

    added method for checking the DB on the stock model

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,6 +1,7 @@
 class UserStocksController < ApplicationController
   def create
-    stock =Stock.new_lookup(params[:ticker])
+    sotck = Stock.check_db(params[:ticker])
+    stock = Stock.new_lookup(params[:ticker])
     @user_stock = UserStock.create(user: current_user, stock: stock)
   end
 end
