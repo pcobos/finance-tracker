271b7b35d27e9b63b8501ec66fa1b22e663aa8d4
Pedro Cobos <pedro.cobos@live.com>
2020-09-26 18:24:53 -0500

    added a condition on create action to check if the stock is blank. Also, added method to check if the Stock already exists on the DB

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,7 +1,10 @@
 class UserStocksController < ApplicationController
+  
   def create
-    sotck = Stock.check_db(params[:ticker])
-    stock = Stock.new_lookup(params[:ticker])
-    @user_stock = UserStock.create(user: current_user, stock: stock)
+    stock = Stock.check_db(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      @user_stock = UserStock.create(user: current_user,  stock: stock)
+    end
   end
 end
