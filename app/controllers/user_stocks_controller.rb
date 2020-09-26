4a1ce87c85d59e511b32b63b2c02076d7a2d0c6c
Pedro Cobos <pedro.cobos@live.com>
2020-09-25 22:38:33 -0500

    added create action to the UserStocksController

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,5 +1,6 @@
 class UserStocksController < ApplicationController
   def create
-    
+    stock =Stock.new_lookup(params[:ticker])
+    @user_stock = UserStock.create(user: current_user, stock: stock)
   end
 end
