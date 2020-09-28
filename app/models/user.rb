53f8c5eadf7d16c2bbae7db110508bbbe2493b73
Pedro Cobos <pedro.cobos@live.com>
2020-09-28 18:58:41 -0500

    added methods for determining if the user can track a certain stock based on the number of stocks tracked and the already tracked ones

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,23 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Following method checks if the user stock count is less than ten, we will use it to determine if he can add more stocks to the portfolio and also to display the proper message in case this is true.
+  def tracking_limit_exceeded?
+    stocks.count < 10
+  end
+
+  def already_tracking?(ticker_symbol)
+    # First we check if the stock already exists in our table, if it doesn't it means that the user is not tracking it for sure. Notice that we are using our Stock.check_db instance method to check in the DB (calling and reusing method):
+    stock = Stock.check_db(ticker: ticker_symbol)
+    # Next condition allows the method to carry on in case there is no stock object with said ticker_symbol:
+    return false unless stock
+    # If we reach this point, it means that the stock exists in the DB, however, we still need to check if the current user is tracking it:
+    stocks.where(id: stock.id).exists?
+  end
+
+  # Finally, this method calls both of our previous methods to determine if the stock can be added to our tracked stocks portfolio
+  def can_track_stock?(ticker_symbol)
+    tracking_limit_exceeded? || !already_tracking?(ticker_symbol)
+  end
 end
