53c85affd2afd0e5ee2052cbb1fdaddeb2fcb380
Pedro Cobos <pedro.cobos@live.com>
2020-09-29 18:02:28 -0500

    added conditional on stock view to prevent the user from adding stock to porfolio if limit is reached or stock is already being tracked

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   def already_tracking?(ticker_symbol)
     # First we check if the stock already exists in our table, if it doesn't it means that the user is not tracking it for sure. Notice that we are using our Stock.check_db instance method to check in the DB (calling and reusing method):
-    stock = Stock.check_db(ticker: ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
     # Next condition allows the method to carry on in case there is no stock object with said ticker_symbol:
     return false unless stock
     # If we reach this point, it means that the stock exists in the DB, however, we still need to check if the current user is tracking it:
@@ -21,6 +21,6 @@ class User < ApplicationRecord
 
   # Finally, this method calls both of our previous methods to determine if the stock can be added to our tracked stocks portfolio
   def can_track_stock?(ticker_symbol)
-    tracking_limit_exceeded? || !already_tracking?(ticker_symbol)
+    tracking_limit_exceeded? && !already_tracking?(ticker_symbol)
   end
 end
