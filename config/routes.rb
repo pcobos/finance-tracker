222a63566e8fb35ce43db627c92b2c61fa16a10c
Pedro Cobos <pedro.cobos@live.com>
2020-09-29 18:14:09 -0500

    added delete route for user stocks

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'users/my_portfolio', to: 'users#my_portfolio', as: "my_portfolio"
   get 'search_stock', to: 'stocks#search'
 
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
 end
