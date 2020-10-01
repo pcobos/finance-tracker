038d43b0f68f70ff0fcc42d7f6b957f050f543de
Pedro Cobos <pedro.cobos@live.com>
2020-09-30 23:32:31 -0500

    added first_name and last_name columns to User, ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,16 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_234244) do
+ActiveRecord::Schema.define(version: 2020_10_01_043015) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -26,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_09_18_234244) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
