1cc65b56025f86ad11e6e97dbf3515598a9d0341
Pedro Cobos <pedro.cobos@live.com>
2020-09-30 23:19:18 -0500

    added alert to confirm delete action and a flash notice message to confirm that the stock was removed

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -12,8 +12,10 @@ class UserStocksController < ApplicationController
   end
 
   def destroy
-    @user_stock = UserStock.find(params[:id])
-    @user_stock.destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id ).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.name} was succesfully removed from portfolio"
 
     redirect_to my_portfolio_path
   end
