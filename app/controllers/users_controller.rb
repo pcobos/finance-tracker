c589b36e8b1390e0589b409944e6e30f42e659cc
Pedro Cobos <pedro.cobos@live.com>
2020-10-05 22:02:40 -0500

    removed unnecessary edit and update actions from user controller, added permitted parameters method to application controller for editing devise user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,4 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-
-    redirect_to my_portfolio_path
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :encrypted_password, :first_name, :last_name)
-  end
 end
