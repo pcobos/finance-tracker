6bf01b944424dc0022d9f750c68155ee1c0408bb
Pedro Cobos <pedro.cobos@live.com>
2020-10-11 21:46:29 -0500

    added search users route, added friendships create and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'users/my_portfolio', to: 'users#my_portfolio', as: "my_portfolio"
   get 'users/friends', to: 'users#friends', as: "friends"
   get 'search_stock', to: 'stocks#search'
+  get 'search_users', to: 'users#search'
 
   resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
 end
