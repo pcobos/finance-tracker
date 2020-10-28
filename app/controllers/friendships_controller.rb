6d81590f7c6f7138dc50ec8392ed230cce4a1e85
Pedro Cobos <pedro.cobos@live.com>
2020-10-28 13:13:32 -0600

    added destroy action to friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,11 @@
 class FriendshipsController < ApplicationController
+  def destroy
+    friendship = Friendship.find(params[:id])
+    friendship.user = current_user
+    friendship.friend = User.find(params[:id])
+    friendship.destroy
+
+    redirect_to friends_path
+  end
+  
 end
