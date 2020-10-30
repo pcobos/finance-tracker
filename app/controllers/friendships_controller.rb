078ac364c248425c824f87a46061e1925e4a0645
Pedro Cobos <pedro.cobos@live.com>
2020-10-30 14:45:08 -0600

    updated #create action on friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,17 +1,19 @@
 class FriendshipsController < ApplicationController
 
   def create
-    @friendship = Friendship.new
-    @friendship.user = current_user
-    @friendship.friend = User.find(params[:friend])
+    @friendship = Friendship.create(user_id: current_user.id, friend_id: params[:friend])
+    # @friendship.user = current_user
+    # @friendship.friend = User.find(params[:friend])
     
+    # friend = User.find(params[:friend])
+    # current_user.friendships.build(friend_id: friend.id)
+    # byebug
     redirect_to friends_path
   end
   
 
   def destroy
-    friendship = Friendship.where(user_id: current_user.id, friend_id: User.find(params[:id])).first
-    friendship.user = current_user
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
 
     redirect_to friends_path
